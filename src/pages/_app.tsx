0dee89f6fdb4cf70991f17b7bef4a4ea36a4d441
sukemaru-injp <72376331+sukemaru-injp@users.noreply.github.com>
2023-03-26 23:44:42 +0900

    gtm-push

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,20 @@
 import '@/styles/globals.css';
+import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { Layout } from '@/common/layout';
+import { useRouter } from 'next/router'
+import { pageview } from '@/libs/gtm';
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', pageview)
+    return () => {
+      router.events.off('routeChangeComplete', pageview)
+    }
+  }, [router.events])
+
   return (
     <>
       <Layout>
